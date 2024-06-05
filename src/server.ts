9ec0a93ef861c94a4180dc80012a1151004497bb
Pascoal Kahamba <pascoalkahamba25@gmail.com>
2024-06-05 19:06:09 +0100

    I've used cors for protete my requests

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { userRoutes } from "./routes/user.routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 const port = process.env.PORT;
 
