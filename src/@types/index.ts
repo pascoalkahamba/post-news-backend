eea849421db7cd3ce2f03e2925fcec69e8ada96e
pascoalkahamba <pascoalkahamba25@gmail.com>
2026-02-16 16:16:52 +0100

    fix: I've just fixed the all conflicts

--- src/@types/index.ts
+++
@@ -1,4 +0,0 @@
-import { User } from "@prisma/client";
-
-export type UserModalT = Pick<User, "name" | "password" | "email">;
-export type JsonWebTokenErrorT = "jwt malformed" | "jwt must be provided";
