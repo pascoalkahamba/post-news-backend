5a8c5fc11895ae49f3a3ba122cc18dce2349fa52
pascoalkahamba <pascoalkahamba25@gmail.com>
2026-02-20 17:48:00 +0100

    feature: I've just created the approch to get all categories

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -5,11 +5,9 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 const categoryRoutes = express.Router();
 const categoryController = new CategoryController();
 
-// Public routes - get all and get by id
 categoryRoutes.get("/", categoryController.getAll);
 categoryRoutes.get("/:id", categoryController.getById);
 
-// Protected routes - create, update, delete
 categoryRoutes.use(authMiddleware);
 categoryRoutes.post("/create", categoryController.create);
 categoryRoutes.put("/update/:id", categoryController.update);
