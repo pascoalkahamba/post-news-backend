74401ff74cc4a3fdfb8d6ec0dcd5d91ba338955c
pascoalkahamba <pascoalkahamba25@gmail.com>
2026-03-15 23:27:43 +0100

    improved: I've just improved the comment and reply approches

--- src/routes/reply.routes.ts
+++ src/routes/reply.routes.ts
@@ -5,12 +5,10 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 const replyRoutes = express.Router();
 const replyController = new ReplyController();
 
-// Public routes
 replyRoutes.get("/", replyController.getAll);
 replyRoutes.get("/comment/:commentId", replyController.getByCommentId);
 replyRoutes.get("/:id", replyController.getById);
 
-// Protected routes
 replyRoutes.use(authMiddleware);
 replyRoutes.post("/create", replyController.create);
 replyRoutes.put("/update/:id", replyController.update);
